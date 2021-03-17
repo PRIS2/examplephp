d4a613093579d19ae1e1f6f91f16947c622ea0e5
imaguila <imaguila@ual.es>
2021-03-17 11:54:53 +0100

    1

--- cond.php
+++ cond.php
@@ -1,5 +1,9 @@
 <?php
 
-if ( isset( $settings['wp-uploads'] ) && $settings['wp-uploads'] && in_array( $key, array( 'copy-to-s3', 'serve-from-s3' ) ) ) {
+if ( upload_is_valid( $settings, $key ) ) {
     return '1';
+}
+
+function upload_is_valid( $settings, $key ) {
+    return isset( $settings['wp-uploads'] ) && $settings['wp-uploads'] && in_array( $key, array( 'copy-to-s3', 'serve-from-s3' ) );
 }
