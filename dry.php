b74b80900fcd12753d130495b5009aec3adba2ac
imaguila <imaguila@ual.es>
2021-03-17 11:39:22 +0100

    11

--- dry.php
+++ dry.php
@@ -1,12 +1,8 @@
 <?php
 
-class AwesomeAddon {
+abstract class Addon {
 
-    private $settings;
-
-    public function __construct( $settings ) {
-        $this->set_settings( $settings );
-    }
+    protected $settings;
 
     protected function set_settings( $settings ) {
         if ( ! is_array( $settings ) ) {
@@ -14,6 +10,14 @@ class AwesomeAddon {
         }
 
         $this->settings = $settings;
+    }
+
+}
+
+class AwesomeAddon extends Addon {
+
+    public function __construct( $settings ) {
+        $this->set_settings( $settings );
     }
 
     protected function do_something_awesome() {
@@ -22,20 +26,10 @@ class AwesomeAddon {
 
 }
 
-class EvenMoreAwesomeAddon {
-
-    private $settings;
+class EvenMoreAwesomeAddon extends Addon {
 
     public function __construct( $settings ) {
         $this->set_settings( $settings );
-    }
-
-    protected function set_settings( $settings ) {
-        if ( ! is_array( $settings ) ) {
-            throw new \Exception( 'Invalid settings' );
-        }
-
-        $this->settings = $settings;
     }
 
     protected function do_something_even_more_awesome() {
